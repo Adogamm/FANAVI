f754e32efaf11d9b157cde511822d98ce90b4ca0
FernandoHernandezVazquez <1719210003@utectulancingo.edu.mx>
2021-11-10 18:52:46 -0600

    Drop Files Function

---
+++ Drop_files/delete.py
@@ -0,0 +1,10 @@
+import os
+from os import remove
+
+# Eliminar archivos despues de su uso
+def DeleteFiles():
+    try:
+        remove(r'C:\\Fanavi\\images\\screenshot.png')
+        print("la captura fue removida con exito")
+    except:
+        print("No se eliminó o no existe")

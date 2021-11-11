fc8cdd4f55fc6a3196f74ce6cc7818a0144cdb2f
FernandoHernandezVazquez <1719210003@utectulancingo.edu.mx>
2021-11-10 18:53:48 -0600

    Function to take the screenshot

---
+++ Screenshot/screenshot.py
@@ -0,0 +1,22 @@
+import os
+from os import remove
+import pyautogui
+from PIL import Image
+import ctypes
+
+#Tomar la captura de pantalla
+def TakeScreenshot():
+    im = pyautogui.screenshot()
+    try:
+        im.save(r'C:\\Fanavi\\images\\screenshot.png')
+        print("La captura se realizó correctamente")
+    except:
+        print("La captura no fue tomada")
+
+
+#Cortar la captura a un area especifica.
+def CropImage():
+    route = r'C:\\Fanavi\\images\\screenshot.png'
+    #TODO Aqui se pasan las medidas de la captura
+    route.crop() 
+    route.save(r'C:\\Fanavi\\images\\cropped.png')
